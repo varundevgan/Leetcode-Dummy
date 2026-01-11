61808df66c2d3e33e4267f51aae514c03ac85878
Varun Devgan <varun.devgan@outlook.com>
2026-01-11 10:02:24 +0530

    this "/" removed

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,11 +22,11 @@ app.use(
 );
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get("/", (req, res) => {
-  res.status(400).json({
-    message: "Hello from server",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.status(400).json({
+//     message: "Hello from server",
+//   });
+// });
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend", "dist")));
