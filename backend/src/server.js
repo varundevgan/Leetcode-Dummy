0052d45650d357eb618387e7cb724b08cdcc0240
Varun Devgan <varun.devgan@outlook.com>
2026-01-11 09:54:21 +0530

    Revert "server url fix"
    This reverts commit 6cb865c41c4f589f0fcccbb598d5ee3695897dd5.
    reuse "/" url

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,8 +22,15 @@ app.use(
 );
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-if (ENV.NODE_ENV) {
+app.get("/", (req, res) => {
+  res.status(400).json({
+    message: "Hello from server",
+  });
+});
+
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend", "dist")));
+
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
