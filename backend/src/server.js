efbb050d00e500e8afa7e4047f0d50ccadb6517d
Varun Devgan <varun.devgan@outlook.com>
2026-01-11 12:14:30 +0530

    running frontend and backend seperately

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import { ENV } from "./lib/env.js";
-import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
@@ -10,8 +9,6 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
-
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(
@@ -22,19 +19,11 @@ app.use(
 );
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-// app.get("/", (req, res) => {
-//   res.status(400).json({
-//     message: "Hello from server",
-//   });
-// });
-
-if (ENV.NODE_ENV) {
-  app.use(express.static(path.join(__dirname, "../frontend", "dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+app.get("/", (req, res) => {
+  res.status(400).json({
+    message: "Hello from server",
   });
-}
+});
 
 const startServer = async () => {
   try {
