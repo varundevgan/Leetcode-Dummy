88324e2385d3296acd9007572d64af76490815cd
Varun Devgan <varun.devgan@outlook.com>
2026-02-09 19:14:04 +0530

    testing

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
